4e788e8e125ca0efb0ccd3eb65be0066a85241ff
Jonathan <jonathan@owlwise.com>
2016-08-10 20:29:55 -0400

    index and webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,7 +16,7 @@ devServer: {
 
 module: {
  loaders: [
-   { test: /.jsx?$/,, exclude: /node_modules/, loader: 'babel-loader',
+   { test: /.jsx?$/, exclude: /node_modules/, loader: 'babel-loader',
       query: { presets:['es2015', 'react'] }
     },
    { test: /\.css$/, loader: 'style-loader!css-loader' },
