af2c62f0d1f40c3d39269db6fab998a01a5f8118
Kim Nilsson <kim@nilsson.jp>
2016-08-20 15:47:54 +0900

    Improve build script formatting

--- build/build.bat
+++ build/build.bat
@@ -6,25 +6,28 @@ if exist "%ProgramFiles%\MSBuild\14.0\bin\MSBuild.exe" set MSBUILD="%ProgramFile
 pushd %~dp0
 
 cd ..\daemon
-echo Building 32-bit service...
+echo * Building 32-bit service...
 %MSBUILD% daemon.vcxproj /nologo /v:q /p:Configuration=Release /p:Platform=Win32
 if %errorlevel% neq 0 goto error
-echo Building 64-bit service...
+echo * Building 64-bit service...
 %MSBUILD% daemon.vcxproj /nologo /v:q /p:Configuration=Release /p:Platform=x64
 if %errorlevel% neq 0 goto error
 
 cd ..\client
-echo Updating Node modules...
+echo * Updating Node modules...
 call npm --loglevel=silent install
 if %errorlevel% neq 0 goto error
-echo Rebuilding Electron modules...
+
+echo * Rebuilding Electron modules...
 del node_modules\nslog\build\Release\nslog.node
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32
 if %errorlevel% neq 0 goto error
-echo Packaging Electron app...
+
+echo * Packaging Electron app...
 call node_modules\.bin\electron-packager.cmd .\ cyphervpn --overwrite --platform=win32 --arch=ia32 --icon=app\img\logo.ico --out=..\out\client\ --prune --asar
 if %errorlevel% neq 0 goto error
 
+echo.
 echo Build successful!
 
 :end
@@ -32,5 +35,6 @@ popd
 exit /b %errorlevel%
 
 :error
+echo.
 echo Build failed with error %errorlevel%!
 goto end
