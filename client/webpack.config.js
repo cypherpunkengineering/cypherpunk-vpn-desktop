63e2aaebe922c6658ff384c8bf60e4cc1f3e48c4
Kim Nilsson <kim@cypherpunk.com>
2016-08-28 01:35:39 +0900

    Try to improve webpack settings to work better in built mode

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,32 +1,35 @@
 var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 
-module.exports = {
+var options = {
   target: 'node',
   entry: {
-  app: ['webpack/hot/dev-server', './app/js/app.js'],
-},
-output: {
-  path: './app/build/',
-  filename: 'bundle.js'
-},
-devServer: {
-  contentBase: '.',
-  publicPath: 'http://localhost:8080/built/'
-},
-
-
-module: {
- loaders: [
-   { test: /.jsx?$/, exclude: /node_modules/, loader: 'babel-loader',
-      query: { presets:['es2015', 'react'] }
-    },
-   { test: /\.css$/, loader: 'style-loader!css-loader' },
-   { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
-   { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/octet-stream'}
- ]
-},
- plugins: [
-   new webpack.HotModuleReplacementPlugin(),
-   new webpack.IgnorePlugin(new RegExp("^(fs|ipc)$"))
- ]
+    app: [ './app/js/app.js' ],
+  },
+  externals: [nodeExternals()],
+  output: {
+    path: __dirname + '/app/build/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: '.',
+    publicPath: 'http://localhost:8080/built/'
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['es2015', 'react'], minified: true, comments: false } },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.less$/, loader: 'style!css!less'},
+      { test: /\.(ttf|otf|woff2?)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?mimetype=application/octet-stream'}
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.IgnorePlugin(new RegExp("^(fs|ipc)$")),
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]
 }
+options.target = webpackTargetElectronRenderer(options);
+module.exports = options;
