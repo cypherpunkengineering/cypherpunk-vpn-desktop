14f7ebefb96e22cb341c77800202ddf505907729
Kim Nilsson <kim@cypherpunk.com>
2016-09-04 16:48:47 +0900

    Fix up webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,16 @@
 const webpack = require('webpack');
 const webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const pkg = require('./package.json');
 
 const ENV = process.env.NODE_ENV || 'development';
+const development = ENV === 'development';
+const production = !development;
+
+const devMap = production ? '' : '?sourceMap';
 
 var options = {
   //target: 'electron',
@@ -24,26 +29,26 @@ var options = {
       path.resolve(__dirname, 'node_modules'),
 			'node_modules'
 		],
-    //extensions: [ '', '.min.js', '.js' ],
-    //alias: Object.keys(pkg.webLibraries).map(f => ({ [f]: '/lib/' + f + '.js' }))
+    extensions: [ '', '.min.js', '.js' ],
 	},
-  //resolveLoader: {
-  //  root: path.resolve(__dirname, 'node_modules')
-  //},
   externals: [
     { 'remote': 'commonjs remote' },
     '../rpc.js', // FIXME: exclude all files outside web/ directory
-    Object.keys(pkg.dependencies || {})
+    Object.keys(pkg.dependencies || {}), // exclude packaged node modules 
+    // make a dict that resolves each webLibrary as key -> './lib/[name].js'
+    (function(){
+      var map = {};
+      for (var key in (pkg.webLibraries || {})) {
+        map[key] = './lib/' + key + '.js';
+      }
+      return map;
+    })(),
 	],
-  //devServer: {
-  //  contentBase: '.',
-  //  publicPath: 'http://localhost:8080/built/'
-  //},
-  //devtool: "source-map",
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /(node_modules|\/~\/|webpack\-dev\-server|socket\.io\-client)/, loader: 'babel' },
-      { test: /\.(css|less)$/, loader: 'style!css!less' /*ExtractTextPlugin.extract('style?sourceMap', 'css?sourceMap!postcss!less?sourceMap')*/ },
+      { test: /\.jsx?$/, exclude: /(node_modules|\/~\/|semantic\/|webpack\-dev\-server|socket\.io\-client|\.min\.js$)/, loader: 'babel' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style' + devMap, 'css' + devMap) },
+      { test: /\.less$/, loader: ExtractTextPlugin.extract('style' + devMap, 'css' + devMap + '!less' + devMap) },
       { test: /\.(ttf|otf|eot|woff2?)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
       { test: /\.(png|gif|svg)$/, loader: 'file' },
     ]
@@ -51,17 +56,17 @@ var options = {
   //postcss: function() { return autoprefixer({ browsers: [ 'Chrome >= 52' ] }); },
   plugins: [
     //new webpack.NoErrorsPlugin(),
-    //new ExtractTextPlugin('style.css', { allChunks: true, disable: ENV !== 'production' }),
+    new ExtractTextPlugin('[name].css', { allChunks: true, disable: development }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify(ENV) } }),
-    new webpack.IgnorePlugin(new RegExp("^(fs|ipc)$")),
+    new webpack.IgnorePlugin(new RegExp("^(fs|ipc)$")), // what is this?
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
       "global.jQuery": "jquery"
     }),
-    new HtmlWebpackPlugin({ title: 'Cypherpunk VPN', filename: 'index.html', inject: 'head', minify: { collapseWhitespace: true } }),
+    new HtmlWebpackPlugin({ title: 'Cypherpunk VPN', filename: 'index.html', inject: 'head' }),
     new webpack.HotModuleReplacementPlugin(),
   ],
   node: { console: false, global: false, process: false, Buffer: false, __filename: false, __dirname: false, setImmediate: false },
