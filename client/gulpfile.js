874837d82a17892515c0fd0c720c03aed3786a87
Kim Nilsson <kim@cypherpunk.com>
2016-09-04 16:44:33 +0900

    Clean up gulpfile.js

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -25,25 +25,95 @@ var webpackConfig = require('./webpack.config');
 
 gulp.task('default', ['build']);
 
-gulp.task('build', ['clean'], function(callback) {
-  runSequence('build-semantic', 'build-webpack', 'build-main', callback);
+gulp.task('build', ['build-app']);
+
+gulp.task('clean', ['clean-app', 'clean-semantic', 'clean-web-libraries']);
+
+gulp.task('watch', ['build'], function() {
+  runSequence([
+    'watch-semantic',
+    'watch-main',
+  ]);
 });
 
-gulp.task('clean', ['clean-web-libraries'], function() {
+
+/***************************************************************************
+ * APP: Perform all tasks necessary to produce the app/ directory.
+ */
+gulp.task('build-app', ['build-app-nodemodules', 'build-main', 'build-web' ]);
+gulp.task('clean-app', function() {
   return gulp.src('app', { read: false })
     .pipe(clean());
 });
 
-// Helper object: call one callback if stream is empty (no files), and another otherwise
-function ifEmpty(isEmpty, isNotEmpty) {
-  var empty = true;
-  return new stream.Transform({
-    objectMode: true,
-    transform: function(chunk, encoding, callback) { if (empty) { isNotEmpty(); empty = false; } callback(null, chunk); },
-    flush: function(callback) { if (empty) { isEmpty(); } callback(); }
-  })
-}
+/***************************************************************************
+ * APP NODE MODULES: Install required node modules in the app/ directory.
+ */
+gulp.task('build-app-nodemodules', ['build-app-packagejson'], function() {
+  return gulp.src('app/package.json')
+    .pipe(install({ production: true }));
+});
+gulp.task('watch-app-nodemodules', function() {
+  gulp.watch('app/package.json', ['build-app-nodemodules']);
+});
 
+/***************************************************************************
+ * APP PACKAGE.JSON: Automatically create the app-specific package.json in
+ * the app/ directory stripping out unwanted parts from the main file.  
+ */
+gulp.task('build-app-packagejson', function() {
+  return gulp.src('package.json')
+    .pipe(jsonTransform(function(data, file) {
+      var result = {};
+      Object.assign(result, data);
+      ['scripts', 'engines', 'devDependencies'].forEach(e => delete result[e]);
+      return result;
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+/***************************************************************************
+ * MAIN: Perform all tasks required to produce the scripts and assets that
+ * will run in the Electron main process (in the app/ directory).  
+ */
+gulp.task('build-main', ['build-main-assets'], function() {
+  return gulp.src(['src/**/*.js', '!src/web', '!src/web/**/*', '!src/assets', '!src/assets/**/*'])
+    .pipe(newer('app'))
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('app'));
+});
+gulp.task('build-main-assets', function() {
+  return gulp.src('src/assets/**/*')
+    .pipe(newer('app/assets'))
+    .pipe(gulp.dest('app/assets'));
+})
+
+/***************************************************************************
+ * WEB: Perform all tasks necessary to produce the app/web/ directory.
+ */
+//gulp.task('build-web', ['build-webpack']);
+gulp.task('build-web', ['build-webpack'], function() {
+  return gulp.src('src/web/lib/**/*')
+    .pipe(gulp.dest('app/web/lib'));
+});
+
+/***************************************************************************
+ * WEBPACK: Bundle up all sources from src/web/ and referenced node modules
+ * and put the resulting output files in app/web/.
+ */
+gulp.task('build-webpack', ['build-semantic', 'build-web-libraries'], function(callback) {
+  webpack(webpackConfig).run(webpackLogger(callback));
+});
+gulp.task('watch-webpack', function() {
+  webpack(webpackConfig).watch({}, webpackLogger(function(){}));
+});
+
+/***************************************************************************
+ * SEMANTIC: Compile the Semantic UI sources from semantic/ and put the
+ * output in src/web/semantic/ (where it can be picked up by webpack).
+ */
 gulp.task('build-semantic', function(callback) {
   gulp.src('semantic/**/*', { read: false })
     .pipe(newer('src/web/semantic/semantic.css')) // this is the last generated file
@@ -57,27 +127,15 @@ gulp.task('build-semantic', function(callback) {
 gulp.task('clean-semantic', function() {
   return gulp.src('src/web/semantic')
     .pipe(clean());
-  //spawn('"../node_modules/.bin/gulp"', [ 'clean', '--force' ], { cwd: 'semantic', shell: true, stdio: 'inherit' }).on('exit', callback);  
+});
+gulp.task('watch-semantic', function(callback) {
+  spawn('"../node_modules/.bin/gulp"', [ 'watch' ], { cwd: 'semantic', shell: true, stdio: 'inherit' })
 });
 
-gulp.task('build-webpack', ['build-semantic'/*,'build-web-libraries'*/], function(callback) {
-  // TODO: Only build if needed?
-  //gulp.src('app/js/app.js')
-  //  .pipe(webpack(webpackConfig))
-  //  .pipe(gulp.dest('build'));
-  webpack(webpackConfig).run(function(err, stats) {
-    if (err) throw new gutil.PluginError('webpack', err);
-    gutil.log("Webpack completed successfully:\n" +
-      stats.toString({
-        hash: false,
-        version: false,
-        cached: false,
-        colors: true
-      }));
-    callback();
-  });
-});
-
+/***************************************************************************
+ * WEB LIBRARIES: Download any javascript libraries mentioned in a special
+ * 'webLibraries' section of package.json and put them in src/web/lib/.
+ */
 gulp.task('build-web-libraries', function() {
   var requests = [];
   for (var file in (packageJson.webLibraries || {})) {
@@ -91,46 +149,37 @@ gulp.task('clean-web-libraries', function() {
     .pipe(clean());
 });
 
-gulp.task('build-app', ['build-app-nodemodules', 'build-main', 'build-web' ]);
-
-gulp.task('build-web', ['build-webpack'], function() {
-  return gulp.src('src/web/lib/**/*')
-    .pipe(gulp.dest('app/web/lib'));
-});
-
-gulp.task('build-app-packagejson', function() {
-  return gulp.src('package.json')
-    .pipe(jsonTransform(function(data, file) {
-      var result = {};
-      Object.assign(result, data);
-      ['scripts', 'engines', 'devDependencies'].forEach(e => delete result[e]);
-      return result;
-    }))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('build-app-nodemodules', ['build-app-packagejson'], function() {
-  return gulp.src('app/package.json')
-    .pipe(install({ production: true }));
-});
-
-gulp.task('build-main', ['build-main-assets'], function() {
-  return gulp.src(['src/**/*.js', '!src/web', '!src/web/**/*', '!src/assets', '!src/assets/**/*'])
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('build-main-assets', function() {
-  return gulp.src('src/assets/**/*')
-    .pipe(gulp.dest('app/assets'));
-})
 
 
-// TODO: Support watch
+/**************************************************************************/
+
+// Helper function: call one callback if stream is empty (no files), and another otherwise.
+function ifEmpty(isEmpty, isNotEmpty) {
+  var empty = true;
+  return new stream.Transform({
+    objectMode: true,
+    transform: function(chunk, encoding, callback) { if (empty) { isNotEmpty(); empty = false; } callback(null, chunk); },
+    flush: function(callback) { if (empty) { isEmpty(); } callback(); }
+  })
+}
+
+// Helper function: print out the results from webpack, or throw an error on failure.
+function webpackLogger(callback) {
+  return function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log("Webpack completed successfully:\n" +
+      stats.toString({
+        hash: false,
+        version: false,
+        cached: false,
+        colors: true
+      }));
+    callback();
+  };
+}
 
 
+// Ignore everything below, just copied from a similar build script and included for reference, not used
 if (false) {
 
 
