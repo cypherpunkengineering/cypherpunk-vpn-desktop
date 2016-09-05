0711343ce1906091672b1a191ed9c2fd22011c0d
Kim Nilsson <kim@cypherpunk.com>
2016-09-05 14:38:08 +0900

    Improve gulpfile.js to support downloading precompiled libraries

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -15,6 +15,8 @@ var jsonTransform = require('gulp-json-transform');
 var babel = require('gulp-babel');
 var install = require('gulp-install');
 var download = require('gulp-download-stream');
+var through = require('through');
+var File = require('vinyl');
 var { fork, spawn, exec } = require('child_process');
 var stream = require('stream');
 var webpack = require('webpack');
@@ -33,6 +35,8 @@ gulp.task('watch', ['build'], function() {
   runSequence([
     'watch-semantic',
     'watch-main',
+    'watch-web',
+    'watch-app-nodemodules',
   ]);
 });
 
@@ -89,15 +93,19 @@ gulp.task('build-main-assets', function() {
     .pipe(newer('app/assets'))
     .pipe(gulp.dest('app/assets'));
 })
+gulp.task('watch-main', function() {
+  gulp.watch(['src/**/*.js', '!src/web', '!src/web/**/*'], ['build-main']);
+});
 
 /***************************************************************************
  * WEB: Perform all tasks necessary to produce the app/web/ directory.
  */
-//gulp.task('build-web', ['build-webpack']);
-gulp.task('build-web', ['build-webpack'], function() {
-  return gulp.src('src/web/lib/**/*')
-    .pipe(gulp.dest('app/web/lib'));
-});
+gulp.task('build-web', ['build-webpack']);
+//gulp.task('build-web', ['build-webpack'], function() {
+//  return gulp.src('src/web/lib/**/*')
+//    .pipe(gulp.dest('app/web/lib'));
+//});
+gulp.task('watch-web', ['watch-webpack']);
 
 /***************************************************************************
  * WEBPACK: Bundle up all sources from src/web/ and referenced node modules
@@ -116,12 +124,13 @@ gulp.task('watch-webpack', function() {
  */
 gulp.task('build-semantic', function(callback) {
   gulp.src('semantic/**/*', { read: false })
-    .pipe(newer('src/web/semantic/semantic.css')) // this is the last generated file
+    .pipe(newer('src/web/semantic/index.js')) // this is the last generated file
     .pipe(ifEmpty(function() {
       gutil.log("Semantic already up to date");
       callback();
     }, function() {
-      spawn('"../node_modules/.bin/gulp"', [ 'build' ], { cwd: 'semantic', shell: true, stdio: 'inherit' }).on('exit', callback);
+      spawn('"../node_modules/.bin/gulp"', [ 'build' ], { cwd: 'semantic', shell: true, stdio: 'inherit' })
+        .on('exit', makeSemanticWebpackShims(callback));
     }));
 });
 gulp.task('clean-semantic', function() {
@@ -139,13 +148,13 @@ gulp.task('watch-semantic', function(callback) {
 gulp.task('build-web-libraries', function() {
   var requests = [];
   for (var file in (packageJson.webLibraries || {})) {
-    requests.push({ file: file + '.js', url: packageJson.webLibraries[file] });
+    requests.push({ file: file + '.min.js', url: packageJson.webLibraries[file] });
   }
   return download(requests)
     .pipe(gulp.dest('src/web/lib'));
 });
 gulp.task('clean-web-libraries', function() {
-  return gulp.src(Object.keys(packageJson.webLibraries || {}).map(m => 'src/web/lib/' + m + '.js'), { read: false })
+  return gulp.src(Object.keys(packageJson.webLibraries || {}).map(m => 'src/web/lib/' + m + '.min.js'), { read: false })
     .pipe(clean());
 });
 
@@ -178,130 +187,30 @@ function webpackLogger(callback) {
   };
 }
 
-
-// Ignore everything below, just copied from a similar build script and included for reference, not used
-if (false) {
-
-
-gulp.task('watch', function() {
-  runSequence(
-    'build',
-    [
-      'watch-src',
-      'watch-vendor'
-    ]
-  );
-});
-
-gulp.task('build', function(callback) {
-  runSequence(
-    'clean:dist',
-    'build-src',
-    'build-vendor',
-    callback
-  );
-});
-
-gulp.task('build-src', [
-  'build-src:js',
-  'build-src:css',
-  'build-src:html'
-]);
-
-gulp.task('build-vendor', [
-  'build-vendor:js',
-  'build-vendor:css',
-  'build-vendor:semantic-ui'
-]);
-
-gulp.task('watch-src', [
-  'watch-src:js',
-  'watch-src:css',
-  'watch-src:html'
-]);
-
-gulp.task('watch-vendor', function() {
-  gulp.watch('bower.json' ['build-vendor']);
-});
-
-gulp.task('watch-src:js', function() {
-  webpack(webpackConfig).watch({}, webpackLogger());
-});
-
-gulp.task('watch-src:css', function() {
-  gulp.watch('./src/less/**/*', ['build-src:css']);
-});
-
-gulp.task('watch-src:html', function() {
-  gulp.watch('./src/html/**/*', ['build-src:html']);
-});
-
-gulp.task('build-src:js', function(callback) {
-  webpack(webpackConfig).run(webpackLogger(callback));
-});
-
-gulp.task('build-src:css', function() {
-  return gulp.src('src/less/main.less')
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(rename('main.css'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build-src:html', function() {
-  return gulp.src('src/html/main.html')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build-vendor:js', function() {
-  return gulp.src(mainBowerFiles())
-    .pipe(filter(['**/*.js']))
-    .pipe(sourcemaps.init())
-    .pipe(concat('vendor.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build-vendor:css', function() {
-  return gulp.src(mainBowerFiles())
-    .pipe(filter(['**/*.css']))
-    .pipe(sourcemaps.init())
-    .pipe(concat('vendor.css'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build-vendor:semantic-ui', function() {
-  return gulp.src('bower_components/semantic-ui/dist/**/*')
-    .pipe(gulp.dest('dist/semantic-ui'));
-});
-
-gulp.task('clean:dist', function() {
-  return gulp.src('dist')
-    .pipe(clean());
-});
-
-gulp.task('build-electron', function(callback) {
-  runSequence(
-    'build',
-    'build-electron:clean',
-    'build-electron:src',
-    callback
-  );
-});
-
-gulp.task('build-electron:clean', function() {
-  return gulp.src('Electron.app')
-    .pipe(clean());
-});
-
-gulp.task('build-electron:src', shell.task([
-  'cp -a node_modules/electron-prebuilt/dist/Electron.app ./',
-  'mkdir Electron.app/Contents/Resources/app',
-  'cp main.js Electron.app/Contents/Resources/app/',
-  'cp package.json Electron.app/Contents/Resources/app/',
-  'cp -a dist Electron.app/Contents/Resources/app/'
-]));
-
+// Helper function: make webpack shims for each js/css file in Semantic UI (so we can just require the component name)
+function makeSemanticWebpackShims(callback) {
+  return function() {
+    gulp.src(['src/web/semantic/**/*.min.js', 'src/web/semantic/**/*.min.css'], { read: false })
+      .pipe((function(){
+        var files = {};
+        function onFile(file) {
+          var f = file.relative.replace('\\', '/');
+          var m = f.replace(/\.min\.(js|css)$/, '');
+          var r = f.replace(/^([^/]*\/)*/, '');
+          (files[m] || (files[m] = [])).push('./' + r);
+        }
+        function onEnd() {
+          for (var e in files) {
+            var n = e.endsWith('semantic') ? 'index.js' : e + '.webpack.js';
+            this.emit('data', new File({
+              cwd: '.', base: 'src/web/semantic', path: 'src/web/semantic/' + n, contents: new Buffer(files[e].sort().map(f => "require('" + f + "');\n").join(''))
+            }));
+          }
+          this.emit('end');
+        }
+        return through(onFile, onEnd);
+      })())
+      .pipe(gulp.dest('src/web/semantic'))
+      .on('end', callback);
+  };
 }
