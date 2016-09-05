696c2ba5fe9c9d689c381b04eccd2803f464c925
Kim Nilsson <kim@cypherpunk.com>
2016-09-05 14:44:28 +0900

    Improve unload helper

--- client/src/web/unload.js
+++ client/src/web/unload.js
@@ -6,36 +6,37 @@ module.exports = window.addUnloadHandler = function addUnloadHandler(fn) {
   unloadHandlers.push(fn);
 };
 
-var oldClose = window.close;
-var doClose = function() { console.log("doClose"); oldClose.call(window); };
+var doClose = window.close.bind(window);
 var exiting = false;
 
 function performCloseChain() {
   var p = Promise.resolve(null);
-  unloadHandlers.forEach(fn => {
-    p = p.then(fn);
-  });
+  unloadHandlers.forEach(fn => { p = p.then(fn); });
   return p
-    .then(() => { doClose(); })
+    .then(doClose)
     .catch(() => { exiting = false; });
 }
 
 // Hook the window.onbeforeunload event (in case the window itself is closed)
 window.addEventListener('beforeunload', e => {
-  console.log("beforeunload");
   if (!exiting) {
     exiting = true;
-    performCloseChain();
-    e.returnValue = false;
-    return false;
+    if (unloadHandlers) {
+      setTimeout(performCloseChain, 0);
+      e.returnValue = false;
+      return false;
+    }
   }
 });
 
 // Hook the window.close function too, to do programmatic shutdown more cleanly
 window.close = function close() {
-  console.log("close");
   if (!exiting) {
     exiting = true;
-    performCloseChain();
+    if (unloadHandlers) {
+      setTimeout(performCloseChain, 0);
+    } else {
+      doClose();
+    }
   }
 };
