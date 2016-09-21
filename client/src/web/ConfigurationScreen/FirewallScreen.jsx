b15e7eb7df3b2700641ce1f6655379e50d2ae987
Kim Nilsson <kim@cypherpunk.com>
2016-09-22 01:18:48 +0900

    Hook up firewall setting to daemon

--- client/src/web/ConfigurationScreen/FirewallScreen.jsx
+++ client/src/web/ConfigurationScreen/FirewallScreen.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router';
+import daemon from '../daemon.js';
 
 
 export default class FirewallScreen extends React.Component  {
+  constructor() {
+    super();
+    this.onDaemonSettingsChanged = this.onDaemonSettingsChanged.bind(this);
+  }
   componentDidMount() {
     $(this.refs.tab).find('.item').tab();
+    $(this.refs.root).find('.ui.checkbox').checkbox({ onChange: this.onChanged.bind(this) });
+    this.onDaemonSettingsChanged();
+    daemon.on('settings', this.onDaemonSettingsChanged);
+  }
+  componentWillUnmount() {
+    daemon.removeListener('settings', this.onDaemonSettingsChanged);
+  }
+  onChanged() {
+    var value = $(this.refs.root).find('input[name=firewall]:checked').val();
+    daemon.post.applySettings({ 'firewall': value });
+  }
+  onDaemonSettingsChanged() {
+    $(this.refs.root).find('#firewall-' + daemon.settings.firewall).parent().checkbox('set checked');
   }
   render() {
     return(
-      <div>
+      <div ref="root">
         <div className="ui fluid inverted borderless icon menu">
           <Link className="item" to="/configuration"><i className="arrow left icon"></i></Link>
           <div className="header item center aligned">Firewall</div>
@@ -17,8 +35,8 @@ export default class FirewallScreen extends React.Component  {
           <div className="row">
             <div className="olive column">
               <div className="ui radio checkbox">
-                <input id="firewall-automatic" type="radio" name="firewall" />
-                <label for="firewall-automatic">Automatic
+                <input id="firewall-auto" type="radio" name="firewall" value="auto" />
+                <label for="firewall-auto">Automatic
                 <small>
                   (Default) - Firewall will be enabled when you connect and disables when you disconnect from a location. It will remain on if your connection suddenly drops
                 </small>
@@ -29,8 +47,8 @@ export default class FirewallScreen extends React.Component  {
           <div className="row">
             <div className="olive column">
             <div className="ui radio checkbox">
-              <input id="firewall-alwayson" type="radio" name="firewall" />
-              <label for="firewall-alwayson">Always on
+              <input id="firewall-on" type="radio" name="firewall" value="on" />
+              <label for="firewall-on">Always on
               <small>
                 Firewall is always on, and cannot be disabled unless
                 You change this setting. You will not have any Internet access when you’re disconnected from Cypherpunk Network.
@@ -42,7 +60,7 @@ export default class FirewallScreen extends React.Component  {
           <div className="row">
             <div className="olive column">
             <div className="ui radio checkbox">
-              <input id="firewall-off" type="radio" name="firewall" />
+              <input id="firewall-off" type="radio" name="firewall" value="off" />
               <label for="firewall-off">
               Off
               <small>Firewall is off.</small>
