ad16c8326b22c63bec1cee67f9c7b80b3c900977
jonathanjdavis <davis@snickers.org>
2016-09-27 19:03:39 -0400

    CSS !important tweaks
    rows and menu / tabs to same height as zeplin

--- client/src/web/ConfigurationScreen.jsx
+++ client/src/web/ConfigurationScreen.jsx
@@ -9,18 +9,18 @@ export default class ConfigurationScreen extends React.Component  {
   render() {
     return(
       <div className="settings_screen">
-        <div className="ui fluid inverted borderless icon menu">
+        <div className="ui fluid inverted borderless icon menu cp_config_header">
           <Link className="item" to="/connect"><i className="arrow left icon"></i></Link>
           <div className="header item center aligned">Configuration</div>
         </div>
-        <div className="ui two item tabular menu" ref="tab">
-          <a class="item active" data-tab="general">General</a>
-          <a class="item" data-tab="advanced">Advanced</a>
+        <div className="ui two item tabular menu cp_config_tabs" ref="tab">
+          <a className="item active" data-tab="general">General</a>
+          <a className="item" data-tab="advanced">Advanced</a>
         </div>
-        <div class="ui tab active tabscroll" data-tab="general">
+        <div className="ui tab active tabscroll" data-tab="general">
           <GeneralSettings />
         </div>
-        <div class="ui tab tabscroll" data-tab="advanced">
+        <div className="ui tab tabscroll" data-tab="advanced">
           <AdvancedSettings />
         </div>
       </div>
@@ -36,12 +36,12 @@ class AdvancedSettings extends React.Component  {
   render() {
     return(
       <div>
-      <div class="ui padded grid">
-        <div class="row">
-          <div class="eleven wide olive column">
+      <div className="ui padded grid">
+        <div className="row cp_row">
+          <div className="eleven wide olive column">
             Protocol
           </div>
-          <div class="five wide olive right aligned column">
+          <div className="five wide olive right aligned column">
             <div className="ui olive button selection dropdown" ref="protocolDropdown">
               <input type="hidden" name="protocol" />
               <i className="dropdown icon"></i>
@@ -53,11 +53,11 @@ class AdvancedSettings extends React.Component  {
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="nine wide olive column">
+        <div className="row cp_row">
+          <div className="nine wide olive column">
             Remote port
           </div>
-          <div class="seven wide olive right aligned column">
+          <div className="seven wide olive right aligned column">
             <div className="ui olive button selection dropdown" ref="reportportDropdown">
               <input type="hidden" name="remoteport" />
               <i className="dropdown icon"></i>
@@ -70,97 +70,97 @@ class AdvancedSettings extends React.Component  {
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="eleven wide olive column">
+        <div className="row cp_row">
+          <div className="eleven wide olive column">
             Local port
             <small>Customize the port used to connect to Cypherpunk</small>
           </div>
-          <div class="five wide olive right aligned column">
+          <div className="five wide olive right aligned column">
             <div className="ui input">
               <input type="text" size="8"/>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="ten wide olive column">
+        <div className="row cp_row">
+          <div className="ten wide olive column">
             Firewall
             <small>Manage all internet connectivity when you are not connected to our network</small>
           </div>
-          <div class="six wide olive right aligned column">
+          <div className="six wide olive right aligned column">
             <Link to="/firewall"><span id="firewall_text">Automatic</span> <i className="chevron right icon"></i></Link>
           </div>
         </div>
-        <div class="row">
-          <div class="thirteen wide olive column">
+        <div className="row cp_row">
+          <div className="thirteen wide olive column">
             <label for="smallpackets">Use small packets
             <small>Optimized packet size for improved connectivity to various router and mobile networks</small>
             </label>
           </div>
-          <div class="three wide olive right aligned column">
-            <div class="ui checkbox">
+          <div className="three wide olive right aligned column">
+            <div className="ui checkbox">
               <input type="checkbox" name="smallpackets" id="smallpackets"/>
               <label />
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="thirteen wide olive column">
+        <div className="row cp_row">
+          <div className="thirteen wide olive column">
             <label for="allowlocaltraffic">Allow local traffic when firewall is on</label>
           </div>
-          <div class="three wide olive right aligned column">
-            <div class="ui checkbox">
+          <div className="three wide olive right aligned column">
+            <div className="ui checkbox">
               <input type="checkbox" name="allowlocaltraffic" id="allowlocaltraffic"/>
               <label />
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="thirteen wide olive column">
+        <div className="row cp_row">
+          <div className="thirteen wide olive column">
             <label for="requestportforwarding">Request port forwarding
             <small>Allow incoming connections to your mobile device over an external port</small>
             </label>
           </div>
-          <div class="three wide olive right aligned column">
-            <div class="ui checkbox">
+          <div className="three wide olive right aligned column">
+            <div className="ui checkbox">
               <input type="checkbox" name="requestportforwarding" id="requestportforwarding"/>
               <label />
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="thirteen wide olive column">
+        <div className="row cp_row">
+          <div className="thirteen wide olive column">
             <label for="ipv6leakprotection">IPv6 leak protection
             <small>Disable IPv6 while using Cypherpunk</small>
             </label>
           </div>
-          <div class="three wide olive right aligned column">
-            <div class="ui checkbox">
+          <div className="three wide olive right aligned column">
+            <div className="ui checkbox">
               <input type="checkbox" name="ipv6leakprotection" id="ipv6leakprotection"/>
               <label />
             </div>
           </div>
         </div>
 
-        <div class="row">
-          <div class="thirteen wide olive column">
+        <div className="row cp_row">
+          <div className="thirteen wide olive column">
             <label for="dnsleakprotection">DNS leak protection
             <small>Prevent leaking DNS queries using Cypherpunk</small>
             </label>
           </div>
-          <div class="three wide olive right aligned column">
-            <div class="ui checkbox">
+          <div className="three wide olive right aligned column">
+            <div className="ui checkbox">
               <input type="checkbox" name="dnsleakprotection" id="dnsleakprotection" />
               <label />
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="nine wide olive column">
+        <div className="row cp_row">
+          <div className="nine wide olive column">
             <Link to="/encryption">Encryption
             <small>Cipher AES-256 Auth SHA512 Key 4096-bit</small>
             </Link>
           </div>
-          <div class="seven wide olive right aligned column">
+          <div className="seven wide olive right aligned column">
             <Link to="/encryption"><span id="encryption_text">Automatic</span> <i className="chevron right icon"></i></Link>
           </div>
         </div>
@@ -180,114 +180,114 @@ class GeneralSettings extends React.Component  {
       <div>
 
 
-        <div class="ui equal width center aligned padded grid">
-          <div class="row">
-            <div class="olive column">
-              <i class="spy icon"></i> Wiz
+        <div className="ui equal width center aligned padded grid ">
+          <div className="row cp_row">
+            <div className="olive column cp_account_avatar">
+              <i className="spy icon"></i> Wiz
             </div>
           </div>
-          <div class="row">
-            <div class="olive column">
+          <div className="row cp_row">
+            <div className="olive column">
               Monthly Premium
             </div>
-            <div class="olive column">
-              <small>Renews On 02/02/2016</small>
+            <div className="olive column">
+              <span className="cp_renew_date">Renews On 02/02/2016</span>
             </div>
           </div>
         </div>
 
-        <div class="ui equal width center aligned padded grid">
-          <div class="row">
-            <div class="column">
-              <button id="upgrade" class="ui inverted button">Upgrade</button>
+        <div className="ui equal width center aligned padded grid">
+          <div className="row cp_row">
+            <div className="column">
+              <button id="upgrade" className="ui inverted button">Upgrade</button>
             </div>
           </div>
         </div>
 
-        <div class="ui padded grid">
-          <div class="row">
-            <div class="sixteen wide column">
-              <h3 className="ui yellow header">ACCOUNT DETAILS</h3>
+        <div className="ui padded grid">
+          <div className="row cp_row">
+            <div className="sixteen wide column">
+              <h3 className="ui yellow header cp_h3">ACCOUNT DETAILS</h3>
             </div>
           </div>
-          <div class="row">
-            <div class="four wide olive column">
+          <div className="row cp_row">
+            <div className="four wide olive column">
               Email
             </div>
-            <div class="twelve wide olive right aligned column">
+            <div className="twelve wide olive right aligned column">
               <Link to="/email">
-              wiz@cypherpunk.com <i class="chevron right icon"></i>
+              wiz@cypherpunk.com <i className="chevron right icon"></i>
               </Link>
             </div>
           </div>
-          <div class="row">
-            <div class="seven wide olive column">
+          <div className="row cp_row">
+            <div className="seven wide olive column">
               <Link to="/password">Password</Link>
             </div>
-            <div class="nine wide olive right aligned column">
-              <Link to="/password"><i class="chevron right icon"></i></Link>
+            <div className="nine wide olive right aligned column">
+              <Link to="/password"><i className="chevron right icon"></i></Link>
             </div>
           </div>
-          <div class="row">
-            <div class="seven wide olive column">
+          <div className="row cp_row">
+            <div className="seven wide olive column">
               <Link to="/help">Help</Link>
             </div>
-            <div class="nine wide olive right aligned column">
-              <Link to="/help"><i class="chevron right icon"></i></Link>
+            <div className="nine wide olive right aligned column">
+              <Link to="/help"><i className="chevron right icon"></i></Link>
             </div>
           </div>
-          <div class="row">
-            <div class="seven wide olive column">
+          <div className="row cp_row">
+            <div className="seven wide olive column">
               <Link to="/login">Logout</Link>
             </div>
-            <div class="nine wide olive right aligned column">
+            <div className="nine wide olive right aligned column">
             </div>
           </div>
         </div>
-        <div class="ui padded grid">
-          <div class="row">
-            <div class="sixteen wide column">
-              <h3 className="ui yellow header">BASIC SETTINGS</h3>
+        <div className="ui padded grid">
+          <div className="row cp_row">
+            <div className="sixteen wide column">
+              <h3 className="ui yellow header cp_h3">BASIC SETTINGS</h3>
             </div>
           </div>
-          <div class="row">
-            <div class="eleven wide olive column">
+          <div className="row cp_row">
+            <div className="eleven wide olive column">
               <label for="startapponstartup">Start application on startup</label>
             </div>
-            <div class="five wide olive right aligned column">
-              <div class="ui checkbox">
+            <div className="five wide olive right aligned column">
+              <div className="ui checkbox">
                 <input type="checkbox" name="startapponstartup" id="startapponstartup"/>
                 <label />
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="eleven wide olive column">
+          <div className="row cp_row">
+            <div className="eleven wide olive column">
               <label for="autoconnect">Auto-connect on launch</label>
             </div>
-            <div class="five wide olive right aligned column">
-              <div class="ui checkbox">
+            <div className="five wide olive right aligned column">
+              <div className="ui checkbox">
                 <input type="checkbox" name="autoconnect" id="autoconnect"/>
                 <label />
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="eleven wide olive column">
+          <div className="row cp_row">
+            <div className="eleven wide olive column">
               <label for="desktopnotifications">Show dekstop notifications</label>
             </div>
-            <div class="five wide olive right aligned column">
-              <div class="ui checkbox">
+            <div className="five wide olive right aligned column">
+              <div className="ui checkbox">
                 <input type="checkbox" id="desktopnotifications" name="desktopnotifications" />
                 <label />
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="nine wide olive column">
+          <div className="row cp_row">
+            <div className="nine wide olive column">
               Show cypherpunk icon in
             </div>
-            <div class="seven wide olive right aligned column">
+            <div className="seven wide olive right aligned column">
             <div className="ui olive button selection dropdown" ref="showinDropdown">
               <input type="hidden" name="showin" />
               <i className="dropdown icon"></i>
