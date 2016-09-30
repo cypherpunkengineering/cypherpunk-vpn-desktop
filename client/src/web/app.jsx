2c6ba61efbda825ea5bccbfc7bd728da0db09ce9
jonathanjdavis <davis@snickers.org>
2016-09-30 08:35:08 -0400

    Updated Account and Config Icons
    Updated Assets for Account and Config Icons on Connection Screen

--- client/src/web/app.jsx
+++ client/src/web/app.jsx
@@ -9,6 +9,9 @@ import { Router, Route, IndexRoute, IndexRedirect, Redirect, Link, hashHistory a
 
 import SpinningImage from './assets/img/bgring3.png';
 import CypherPunkLogo from './assets/img/cp_logo1.png';
+import InfoIcon from './assets/img/icon_info.svg';
+import SettingsIcon from './assets/img/icon_settings.svg';
+
 
 import daemon from './daemon.js';
 
@@ -66,9 +69,9 @@ class Titlebar extends React.Component {
   render() {
     return(
       <div id="titlebar" className="ui fixed borderless icon menu">
-        <Link className="item" to="/status"><i className="info circle icon"></i></Link>
+        <Link className="item" to="/status"><img src={InfoIcon} height="22" width="22" /></Link>
         <div className="header item" style={{ flexGrow: 1, justifyContent: "center" }}>Cypherpunk</div>
-        <Link className="item" to="/configuration"><i className="setting icon"></i></Link>
+        <Link className="item" to="/configuration"><img src={SettingsIcon} height="22" width="22" /></Link>
       </div>
     );
   }
