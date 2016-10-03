6d749434469f2eaa5273acea4bddc08db45eba74
Kim Nilsson <kim@cypherpunk.com>
2016-10-04 01:57:32 +0900

    Hook up the encryption setting screen

--- client/src/web/ConfigurationScreen/EncryptionScreen.jsx
+++ client/src/web/ConfigurationScreen/EncryptionScreen.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router';
+import daemon from '../daemon.js';
+
 
 export default class EncryptionScreen extends React.Component  {
+  constructor() {
+    super();
+    this.onDaemonSettingsChanged = this.onDaemonSettingsChanged.bind(this);
+  }
+  componentDidMount() {
+    $(this.refs.root).find('.ui.checkbox').checkbox({ onChange: this.onChanged.bind(this) });
+    this.onDaemonSettingsChanged();
+    daemon.on('settings', this.onDaemonSettingsChanged);
+  }
+  componentWillUnmount() {
+    daemon.removeListener('settings', this.onDaemonSettingsChanged);
+  }
+  onChanged() {
+    var value = $(this.refs.root).find('input[name=encryption]:checked').val();
+    daemon.post.applySettings({ 'encryption': value });
+  }
+  onDaemonSettingsChanged() {
+    $(this.refs.root).find('#encryption-' + daemon.settings.encryption).parent().checkbox('set checked');
+  }
   render() {
     return(
-      <div>
+      <div ref="root">
         <div className="ui fluid inverted borderless icon menu">
           <Link className="item" to="/configuration"><i className="arrow left icon"></i></Link>
           <div className="header item center aligned">Encryption</div>
@@ -13,10 +34,10 @@ export default class EncryptionScreen extends React.Component  {
           <div className="row">
             <div className="olive column">
               <div className="ui radio checkbox">
-                <input id="encryption-automatic" type="radio" name="encryption" />
-                <label for="encryption-automatic">Automatic
+                <input id="encryption-default" type="radio" name="encryption" value="default" />
+                <label for="encryption-default">Automatic
                 <small>
-                  Best combination of speed and security.
+                  Good combination of speed and security.
                 </small>
                 </label>
               </div>
@@ -25,7 +46,7 @@ export default class EncryptionScreen extends React.Component  {
           <div className="row">
             <div className="olive column">
             <div className="ui radio checkbox">
-              <input id="encryption-none" type="radio" name="encryption" />
+              <input id="encryption-none" type="radio" name="encryption" value="none" />
               <label for="encryption-none">None
               <small>
                 Fastest speed but lower security.
@@ -37,7 +58,7 @@ export default class EncryptionScreen extends React.Component  {
           <div className="row">
             <div className="olive column">
             <div className="ui radio checkbox">
-              <input id="encryption-strong" type="radio" name="encryption" />
+              <input id="encryption-strong" type="radio" name="encryption" value="strong" />
               <label for="encryption-strong">Strong
               <small>Higher security but slower speed.</small>
               </label>
@@ -47,9 +68,9 @@ export default class EncryptionScreen extends React.Component  {
           <div className="row">
             <div className="olive column">
             <div className="ui radio checkbox">
-              <input id="encryption-stealth" type="radio" name="encryption" />
+              <input id="encryption-stealth" type="radio" name="encryption" value="stealth" />
               <label for="encryption-stealth">Stealth
-              <small>Proprietary Encryption best for bypassing restrictive network firewalls such as The Great Firewall of China.</small>
+              <small>Suitable for restrictive network environments.</small>
               </label>
             </div>
             </div>
