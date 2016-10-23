eb47cc4d68ad0dc37fcffac0589c3fc92da7f2a4
Ryan Uesato <ruesato@gmail.com>
2016-10-23 01:34:15 -1000

    Updating layout / styles of Account screen

--- client/src/web/components/AccountScreen.js
+++ client/src/web/components/AccountScreen.js
@@ -5,22 +5,18 @@ export default class AccountScreen extends React.Component  {
   render() {
     return(
       <div>
-          <div className="container__comp">
-            <div className="ui fluid inverted borderless icon menu cp_config_header">
+          <div className="container__comp account">
+            <div className="ui fluid borderless icon menu cp_config_header">
               <Link className="item" to="/connect"><i className="angle left icon"></i></Link>
               <div className="header item center aligned">Account</div>
             </div>
             <div className="ui equal width center aligned padded grid ">
               <div className="row cp_row">
-                <div className="olive column cp_account_avatar">
+                <div className="column cp_account_avatar">
                   <i className="spy icon"></i> Wiz
                 </div>
-              </div>
-              <div className="row cp_row">
-                <div className="olive column">
+                <div className="column cp_account_stats">
                   Monthly Premium
-                </div>
-                <div className="olive column">
                   <span className="cp_renew_date">Renews On 02/02/2016</span>
                 </div>
               </div>
@@ -56,6 +52,11 @@ export default class AccountScreen extends React.Component  {
                 </div>
                 <div className="nine wide olive right aligned column">
                   <Link to="/password"><i className="angle right icon"></i></Link>
+                </div>
+              </div>
+              <div className="row cp_row">
+                <div className="sixteen wide column">
+                  <h3 className="ui yellow header cp_h3">More</h3>
                 </div>
               </div>
               <div className="row cp_row">
