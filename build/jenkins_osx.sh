f70860efd4a4a3ba477453339c71b9bd767872d3
J. Maurice <j@wiz.biz>
2016-10-29 19:46:51 +0900

    Fix MacOS codesign keychain woes

--- build/jenkins_osx.sh
+++ build/jenkins_osx.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+
+# fix cwd
 cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# use nvm to set nodejs version
 export NVM_DIR="$HOME/.nvm"
 source "$(brew --prefix nvm)/nvm.sh"
 nvm use v6.8.0
+
+# select and unlock keychain for signing
+KEYCHAIN="cypherpunk.keychain-db"
+# sudo security list-keychains -d common -s "${KEYCHAIN}"
+# security find-identity # should list signing identities
+security default-keychain -s "${KEYCHAIN}"
+security unlock-keychain -p cypherpunkkeychainpassword "${KEYCHAIN}"
+
+# call macos build script
 ./build_osx.sh
+
+# archive package artifact
 PKG="../out/CypherpunkVPN.pkg"
 ARTIFACT="../`printf 'cypherpunk-vpn-macos-%05d' ${BUILD_NUMBER}`.pkg"
 mv "${PKG}" "${ARTIFACT}"
+
+# done
 exit 0
