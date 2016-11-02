e689d64595fad09e10f437a0ecf60f964fe4339c
Ryan Uesato <ruesato@gmail.com>
2016-11-01 15:06:35 -1000

    Refining the Email child screen
    proper positioning of page elements
    cleaner header
    cleaner form field
    positioned action button on bottom edge of panel

--- client/src/web/components/config/EmailScreen.jsx
+++ client/src/web/components/config/EmailScreen.jsx
@@ -4,12 +4,37 @@ import { Link } from 'react-router';
 export default class EmailScreen extends React.Component  {
   render() {
     return(
-      <div class="container__comp">
-        <div className="ui fluid inverted borderless icon menu cp_config_header">
+      <div class="container__comp content material">
+        <div className="ui fluid inverted borderless icon menu cp_config_header titlebar">
           <Link className="item" to="/account"><i className="angle left icon"></i></Link>
           <div className="header item center aligned">Update Email</div>
         </div>
-        <div class="ui padded grid">
+
+        <div className="cp-form form-email">
+          <div className="form-email__new row">
+              <div className="form__field">
+                <i className="icon envelope"></i>
+                <input type="text" placeholder="New email address" />
+              </div>
+          </div>
+          <div class="form-email__confirm row">
+            <div className="form__field">
+              <i className="icon envelope"></i>
+              <input type="text" placeholder="Confirm email address" />
+            </div>
+          </div>
+          <div class="form-email__password row">
+            <div className="form__field">
+              <i className="icon lock"></i>
+              <input type="password" placeholder="Current Password" />
+            </div>
+          </div>
+          <div class="actions__form">
+            <button id="update" class="ui button">Update</button>
+          </div>
+        </div>
+
+        {/*<div class="ui padded grid">
           <div class="row">
             <div class="seven wide olive column">
               New Email
@@ -45,8 +70,8 @@ export default class EmailScreen extends React.Component  {
               <button id="update" class="ui button">Update</button>
             </div>
           </div>
+        </div> */}
 
-        </div>
       </div>
     );
   }
