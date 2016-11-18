3c33902d8421993c2bd35a48bc4005d404d3da78
Kim Nilsson <kim@cypherpunk.com>
2016-11-18 15:04:12 +0900

    Use same build upload location on win/osx builds

--- build/win/jenkins.bat
+++ build/win/jenkins.bat
@@ -14,7 +14,7 @@ cmd /c build.bat clean
 if %errorlevel% neq 0 goto error
 
 cd ..\..\out\win
-scp -scp -P 92 -i "%USERPROFILE%\.ssh\pscp.ppk" cypherpunk-*.exe "upload@builds.cypherpunk.engineering:/data/builds/"
+scp -scp -P 92 -i "%USERPROFILE%\.ssh\pscp.ppk" cypherpunk-*.exe "upload@builds-upload.cypherpunk.engineering:/data/builds/"
 if %errorlevel% equ 0 goto upload_success
 echo * Warning: failed to upload build
 set errorlevel=0
