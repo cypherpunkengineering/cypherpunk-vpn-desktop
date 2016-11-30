9001cb128a906be82d1bb2c183cfc87c276dc91a
J. Maurice <j@wiz.biz>
2016-12-01 04:02:23 +0900

    Fix jekins windows build signing

--- build/win/jenkins.bat
+++ build/win/jenkins.bat
@@ -13,11 +13,17 @@ if %errorlevel% neq 0 goto error
 cmd /c build.bat clean
 if %errorlevel% neq 0 goto error
 
+echo cd to output dir
+cd ..\..\out\win
+
 echo signing build
 "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /debug /f "C:\cypherpunk\cyp-codesign.p12" /p %CODESIGNPW% cypherpunk-*.exe
 
-cd ..\..\out\win
+echo uploading build
 scp -scp -P 92 -i "%USERPROFILE%\.ssh\pscp.ppk" cypherpunk-*.exe "upload@builds-upload.cypherpunk.engineering:/data/builds/"
+
+echo done
+
 if %errorlevel% equ 0 goto upload_success
 echo * Warning: failed to upload build
 set errorlevel=0
