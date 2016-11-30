f05550e0d54056932d47eea92c234f5a3092a401
J. Maurice <j@wiz.biz>
2016-12-01 03:34:05 +0900

    Add windows codesign command to jenkins build script

--- build/win/jenkins.bat
+++ build/win/jenkins.bat
@@ -13,6 +13,9 @@ if %errorlevel% neq 0 goto error
 cmd /c build.bat clean
 if %errorlevel% neq 0 goto error
 
+echo signing build
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /debug /f "C:\cypherpunk\cyp-codesign.p12" /p %CODESIGNPW% cypherpunk-*.exe
+
 cd ..\..\out\win
 scp -scp -P 92 -i "%USERPROFILE%\.ssh\pscp.ppk" cypherpunk-*.exe "upload@builds-upload.cypherpunk.engineering:/data/builds/"
 if %errorlevel% equ 0 goto upload_success
