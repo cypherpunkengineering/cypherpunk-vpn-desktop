f39f10b307b56a1cfc7513131dca9f10d395b778
J. Maurice <j@wiz.biz>
2016-12-22 16:16:53 +0900

    Update crypto profile definitions

--- client/src/web/components/config/PrivacyScreen.jsx
+++ client/src/web/components/config/PrivacyScreen.jsx
@@ -32,7 +32,7 @@ export default class PrivacyScreen extends DaemonAware(React.Component)  {
                 <input type="radio" name="encryption" value="default" id="encryption-default"/>
                 <label>Recommended Default
                 <small>Good balance of speed and privacy.</small>
-                <div className="encryption-details"><span data-title="Cipher">AES-128-CBC</span><span data-title="Auth">SHA-256</span><span data-title="Key">RSA-4096</span></div>
+                <div className="encryption-details"><span data-title="Cipher">AES-128-GCM</span><span data-title="Auth">SHA-256</span><span data-title="Key">RSA-4096</span></div>
                 </label>
               </div>
             </div>
@@ -41,7 +41,7 @@ export default class PrivacyScreen extends DaemonAware(React.Component)  {
                 <input type="radio" name="encryption" value="none" id="encryption-none"/>
                 <label>Max Speed
                 <small>Fastest speed by using no additional encryption.</small>
-                <div className="encryption-details"><span data-title="Cipher">NONE</span><span data-title="Auth">SHA-1</span><span data-title="Key">RSA-4096</span></div>
+                <div className="encryption-details"><span data-title="Cipher">NONE</span><span data-title="Auth">SHA-256</span><span data-title="Key">RSA-4096</span></div>
                 </label>
               </div>
             </div>
@@ -50,7 +50,7 @@ export default class PrivacyScreen extends DaemonAware(React.Component)  {
                 <input type="radio" name="encryption" value="strong" id="encryption-strong"/>
                 <label>Max Privacy
                 <small>Higher security by using stronger encryption.</small>
-                <div className="encryption-details"><span data-title="Cipher">AES-256-CBC</span><span data-title="Auth">SHA-512</span><span data-title="Key">RSA-4096</span></div>
+                <div className="encryption-details"><span data-title="Cipher">AES-256-GCM</span><span data-title="Auth">SHA-256</span><span data-title="Key">RSA-4096</span></div>
                 </label>
               </div>
             </div>
@@ -59,7 +59,7 @@ export default class PrivacyScreen extends DaemonAware(React.Component)  {
                 <input type="radio" name="encryption" value="stealth" id="encryption-stealth"/>
                 <label>Max Stealth
                 <small>Anti-censorship stealth encryption.</small>
-                <div className="encryption-details"><span data-title="Cipher">AES-128-CBC</span><span data-title="Auth">SHA-256</span><span data-title="Key">RSA-4096</span></div>
+                <div className="encryption-details"><span data-title="Cipher">AES-128-GCM</span><span data-title="Auth">SHA-256</span><span data-title="Key">RSA-4096</span></div>
                 </label>
               </div>
             </div>
