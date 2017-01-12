1da2566773f4550cbf6f420632065fc8eefaae71
Kim Nilsson <kim@cypherpunk.com>
2017-01-12 20:46:44 +0900

    Fix up the share screen

--- client/src/web/components/account/ShareScreen.jsx
+++ client/src/web/components/account/ShareScreen.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { SecondaryTitlebar } from '../Titlebar';
 const { shell } = require('electron').remote;
 
-export default class EmailScreen extends React.Component  {
+const FACEBOOK_HREF = 'https://www.facebook.com/sharer/sharer.php?u=cypherpunk.com';
+const TWITTER_HREF = 'https://twitter.com/home?status=Checkout%20cypherpunk.com';
+const EMAIL_HREF = 'mailto:?subject=Checkout Cypherpunk.com&body=I\'m using Cypherpunk\'s VPN and security products. You should too!';
+
+export default class ShareScreen extends React.Component  {
   render() {
     return(
-      <div class="container__comp material">
-        <div className="ui fluid inverted borderless icon menu cp_config_header titlebar">
-          <Link className="nondraggable item" to="/account"><i className="angle left icon"></i></Link>
-          <div className="header item center aligned">Share</div>
-        </div>
-
-        <div className="cp-panel__rows">
-          <div className="link__share--fb row">
-            <a onClick={() => { shell.openExternal('https://www.facebook.com/sharer/sharer.php?u=cypherpunk.com')}}>
-              <i className="icon facebook"></i>
+      <div className="panel" ref="root" id="account-share-panel">
+        <SecondaryTitlebar title="Share" back="/account"/>
+        <div className="content">
+          <div className="pane" style={{ padding: '1em', fontSize: '16px' }}>
+            Like Cypherpunk Privacy?<br/>
+            Please share with your friends!
+          </div>
+          <div className="share pane">
+            <a onClick={() => { shell.openExternal(FACEBOOK_HREF)}}>
+              <i className="facebook icon"/>
               Facebook
             </a>
-          </div>
-          <div className="link__share--twitter row">
-            <a onClick={() => { shell.openExternal('https://twitter.com/home?status=Checkout%20cypherpunk.com')}}>
-              <i className="icon twitter"></i>
+            <a onClick={() => { shell.openExternal(TWITTER_HREF)}}>
+              <i className="twitter icon"/>
               Twitter
-              </a>
-          </div>
-          <div className="link__share--email row">
-            <a onClick={() => { shell.openExternal('mailto:?subject=Checkout Cypherpunk.com&body=I\'m using Cypherpunk\'s VPN and security products. You should too!')}}>
-              <i className="icon envelope"></i>
+            </a>
+            <a onClick={() => { shell.openExternal(EMAIL_HREF)}}>
+              <i className="envelope icon"/>
               Email
             </a>
           </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
