0d80756c050786ab49eb81fb85d83857f023f174
Kim Nilsson <kim@cypherpunk.com>
2017-02-24 06:28:14 +0900

    Update quick panel to use new daemon features

--- client/src/web/components/QuickPanel.js
+++ client/src/web/components/QuickPanel.js
@@ -13,9 +13,9 @@ export class QuickPanel extends DaemonAware(React.Component) {
   constructor(props) {
     super(props);
     this.daemonSubscribeState({
-      config: { locations: {}, regions: {}, countryNames: {}, regionNames: {}, regionOrder: {} },
-      settings: { location: {}, locationFlag: {}, favorites: { filter: v => Array.toDict(v, f => f, f => true) }, lastConnected: {} },
-      state: { pingStats: { onChange: stats => this.checkPingStats(stats) } },
+      config: { locations: true, regions: true, countryNames: true, regionNames: true, regionOrder: true },
+      settings: { location: true, locationFlag: true, favorites: v => { favorites: Array.toDict(v, f => f, f => true) }, lastConnected: true, overrideDNS: true },
+      state: { pingStats: true },
     });
   }
   state = {
@@ -26,39 +26,34 @@ export class QuickPanel extends DaemonAware(React.Component) {
     custom1: null,
     custom2: null,
   }
-  daemonSettingsChanged(settings) {
+  daemonDataChanged(state) {
+    let fastest = null, fastestUS = null, fastestUK = null;
+    let locations = state.locations;
+    Object.forEach(state.pingStats, (id, ping) => {
+      if (ping.replies) {
+        if (locations[id] && (!fastest || ping.average < state.pingStats[fastest].average)) fastest = id;
+        if (locations[id] && locations[id].country.toLowerCase() == 'us' && (!fastestUS || ping.average < state.pingStats[fastestUS].average)) fastestUS = id;
+        if (locations[id] && locations[id].country.toLowerCase() == 'gb' && (!fastestUK || ping.average < state.pingStats[fastestUK].average)) fastestUK = id;
+      }
+    });
     let selected = 6; // other
-    switch (this.state.locationFlag) {
-      case 'cypherplay': selected = 0; break;
+    switch (state.locationFlag) {
+      case 'cypherplay': if (state.overrideDNS) { selected = 0; break; } // fallthrough
       case 'fastest': selected = 1; break;
       case 'fastest-us': selected = 2; break;
       case 'fastest-uk': selected = 3; break;
       default:
-        if (this.state.location === this.state.custom1) selected = 4;
-        else if (this.state.location === this.state.custom2) selected = 5;
+        if (state.location === state.custom1) selected = 4;
+        else if (state.location === state.custom2) selected = 5;
         break;
     }
-    if (selected !== this.state.selected) this.setState({ selected: selected });
-  }
-
-  checkPingStats(stats) {
-    let fastest = null, fastestUS = null, fastestUK = null;
-    let locations = this.state.locations;
-    Object.forEach(stats, (id, ping) => {
-      if (ping.replies) {
-        if (locations[id] && (!fastest || ping.average < stats[fastest].average)) fastest = id;
-        if (locations[id] && locations[id].country.toLowerCase() == 'us' && (!fastestUS || ping.average < stats[fastestUS].average)) fastestUS = id;
-        if (locations[id] && locations[id].country.toLowerCase() == 'gb' && (!fastestUK || ping.average < stats[fastestUK].average)) fastestUK = id;
-      }
-    });
-    // TODO: Disable buttons when stats are unavailable
-    return { fastest, fastestUS, fastestUK, pingStats: stats };
+    return { fastest, fastestUS, fastestUK, selected };
   }
 
   onClick(button, event) {
     if (button === 6) {
       // Show full location list
-    } else if (button != this.state.selected && !event.target.classList.contains('disabled')) {
+    } else if (/*button != this.state.selected &&*/ !event.currentTarget.classList.contains('disabled')) {
       let settings = { locationFlag: '' };
       switch (button) {
         case 0: settings.location = this.state.fastest; settings.locationFlag = 'cypherpunk'; break;
@@ -72,8 +67,6 @@ export class QuickPanel extends DaemonAware(React.Component) {
         console.error("Can't connect - no known fastest server");
         return;
       }
-      this.setState({ selected: button });
-      return;
       daemon.call.applySettings(settings).then(() => {
         daemon.post.connect();
       });
@@ -86,16 +79,16 @@ export class QuickPanel extends DaemonAware(React.Component) {
         <div className="description">CONNECT TO</div>
         <Location className="selected-location" location={this.state.locations[this.state.location]}/>
         <div className="grid">
-          <div className={classList("cypherplay", { "selected": this.state.selected === 0 })} onClick={e => this.onClick(0, e)}>
+          <div className={classList("cypherplay", { "selected": this.state.selected === 0, "disabled": !this.state.fastest || !this.state.overrideDNS })} onClick={e => this.onClick(0, e)}>
             <RetinaImage src={CypherPlayIcon}/><span>CypherPlay&trade;</span>
           </div>
-          <div className={classList("fastest", { "selected": this.state.selected === 1 })} onClick={e => this.onClick(1, e)}>
+          <div className={classList("fastest", { "selected": this.state.selected === 1, "disabled": !this.state.fastest })} onClick={e => this.onClick(1, e)}>
             <RetinaImage src={FastestIcon}/><span>Fastest</span>
           </div>
-          <div className={classList("fastest-us", { "selected": this.state.selected === 2 })} onClick={e => this.onClick(2, e)}>
+          <div className={classList("fastest-us", { "selected": this.state.selected === 2, "disabled": !this.state.fastestUS })} onClick={e => this.onClick(2, e)}>
             <Flag country="us"/><span>Fastest US</span>
           </div>
-          <div className={classList("fastest-uk", { "selected": this.state.selected === 3 })} onClick={e => this.onClick(3, e)}>
+          <div className={classList("fastest-uk", { "selected": this.state.selected === 3, "disabled": !this.state.fastestUK })} onClick={e => this.onClick(3, e)}>
             <Flag country="gb"/><span>Fastest UK</span>
           </div>
           <div className={classList("favorite disabled", { "selected": this.state.selected === 4 })} onClick={e => this.onClick(4, e)}>
