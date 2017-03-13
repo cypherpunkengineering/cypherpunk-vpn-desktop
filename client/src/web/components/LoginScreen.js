60ba2504a5b318f5da38d3d68b30d7e740f7155b
Kim Nilsson <kim@cypherpunk.com>
2017-03-13 19:18:33 +0900

    Disable tutorials until they can be clicked through properly

--- client/src/web/components/LoginScreen.js
+++ client/src/web/components/LoginScreen.js
@@ -76,7 +76,7 @@ function setAccount(data) {
       return Promise.all([ refreshRegionList(), refreshLocationList() ]).then(() => {
         // TODO: Move to Application.onLoginSessionEstablished()
         if (History.getCurrentLocation().pathname.startsWith('/login')) {
-          if (!daemon.settings.seenTutorial) {
+          if (false && !daemon.settings.seenTutorial) {
             History.push('/tutorial/0');
             daemon.post.applySettings({ seenTutorial: true });
           } else {
