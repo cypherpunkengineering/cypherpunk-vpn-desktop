197db82bfd8541ae3b85c3abdb602119cd1fc9fc
Kim Nilsson <kim@cypherpunk.com>
2017-03-21 19:04:14 +0900

    Guard behind setting

--- client/src/app.js
+++ client/src/app.js
@@ -120,8 +120,11 @@ eventPromise(app, 'ready').then(() => {
   let lastState = 'DISCONNECTED';
   daemon.on('state', state => {
     if (state.state && state.state !== lastState) {
-      if (state.state === 'DISCONNECTED') {
-        new Notification("Connection failure", { body: "Your connection to the Cypherpunk network has failed." + ((daemon.settings.firewall !== 'off') ? " Your internet connection has been cut to preventing your traffic from leaking." : "") });
+      if (daemon.settings.showNotifications) {
+        if (state.state === 'DISCONNECTED') {
+          new Notification("Connection failure", { body: "Your connection to the Cypherpunk network has failed." + ((daemon.settings.firewall !== 'off') ? " Your internet connection has been cut to preventing your traffic from leaking." : "") });
+        }
+        }
       }
       lastState = state.state;
     }
