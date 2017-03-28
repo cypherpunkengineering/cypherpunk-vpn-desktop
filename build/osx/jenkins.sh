92ccb89fc93220e25bc2306fbe636b0a9385e54b
Kim Nilsson <kim@cypherpunk.com>
2017-03-28 23:07:16 +0900

    DEBUG: make build terminate early for testing

--- build/osx/jenkins.sh
+++ build/osx/jenkins.sh
@@ -31,6 +31,9 @@ export INSTALLER_SIGN_IDENTITY='"Developer ID Installer: Cypherpunk Partners, sl
 # Invoke build script
 make vpn great again
 
+echo "Finished bulding $(cat ../../version.txt)"
+exit 0
+
 # Archive build artifacts
 cd ../../out/osx/
 ARTIFACT=$(echo cypherpunk-*.pkg)
