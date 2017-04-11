9b0d672a2ca33b04483830f25b75f23ebea28a74
Kim Nilsson <kim@cypherpunk.com>
2017-04-11 23:45:08 +0900

    Add temporary test code

--- client/src/web/components/ConnectScreen.js
+++ client/src/web/components/ConnectScreen.js
@@ -24,6 +24,46 @@ const transitionMap = {
     },
 };
 
+const GPS = {
+  'amsterdam': { lat: -52.3702, long: 4.8952, scale: 1.5 },
+  'atlanta': { lat: -33.7490, long: -84.3880, scale: 1 },
+  'chennai': { lat: -13.0827, long: 80.2707, scale: 1 },
+  'chicago': { lat: -41.8781, long: -87.6298, scale: 1 },
+  'dallas': { lat: -32.7767, long: -96.7970, scale: 1 },
+  'devhonolulu': { lat: -21.3069, long: -157.8533, scale: 4.0 },
+  'devkim': { lat: -35.6895, long: 139.6917, scale: 1.5 },
+  'devtokyo1': { lat: -35.6895, long: 139.6917, scale: 1.5 },
+  'devtokyo3': { lat: -35.6895, long: 139.6917, scale: 1.5 },
+  'devtokyo4': { lat: -35.6895, long: 139.6917, scale: 1.5 },
+  'frankfurt': { lat: -50.1109, long: 8.6821, scale: 1.5 },
+  'hongkong': { lat: -22.3964, long: 114.1095, scale: 1.5 },
+  'istanbul': { lat: -41.0082, long: 28.9784, scale: 1.5 },
+  'london': { lat: -51.5074, long: 0.1278, scale: 1.5 },
+  'losangeles': { lat: -34.0522, long: -118.2437, scale: 1 },
+  'melbourne': { lat: 37.8136, long: 144.9631, scale: 1.5 },
+  'miami': { lat: -25.6717, long: -80.1918, scale: 1 },
+  'milan': { lat: -45.4654, long: 9.1859, scale: 1.5 },
+  'montreal': { lat: -45.5017, long: -73.5673, scale: 1 },
+  'moscow': { lat: -55.7558, long: 37.6173, scale: 1 },
+  'newjersey': { lat: -40.0583, long: -74.4057, scale: 1 },
+  'newyork': { lat: -40.7128, long: -74.0059, scale: 1 },
+  'oslo': { lat: -59.9139, long: 10.7522, scale: 1.5 },
+  'paris': { lat: -48.8566, long: 2.3522, scale: 1.5 },
+  'phoenix': { lat: -33.4484, long: -112.0740, scale: 1 },
+  'saltlakecity': { lat: -40.7608, long: -111.8910, scale: 1 },
+  'saopaulo': { lat: 23.5505, long: -46.6333, scale: 1 },
+  'seattle': { lat: -47.6062, long: -122.3321, scale: 1 },
+  'siliconvalley': { lat: -37.3875, long: -122.0575, scale: 1 },
+  'singapore': { lat: -1.3521, long: 103.8198, scale: 1.5 },
+  'stockholm': { lat: -59.3293, long: 18.0686, scale: 1.5 },
+  'sydney': { lat: 33.8688, long: 151.2093, scale: 1.5 },
+  'tokyo': { lat: -35.6895, long: 139.6917, scale: 1.5 },
+  'toronto': { lat: -43.6532, long: -79.3832, scale: 1 },
+  'vancouver': { lat: -49.2827, long: -123.1207, scale: 1 },
+  'washingtondc': { lat: -38.9072, long: -77.0369, scale: 1 },
+  'zurich': { lat: -47.3769, long: 8.5417, scale: 1.5 },
+};
+
 const AccountIcon = { [1]: require('../assets/img/account_icon.png'), [2]: require('../assets/img/account_icon@2x.png') };
 
 function humanReadableSize(count) {
@@ -151,7 +191,7 @@ export default class ConnectScreen extends DaemonAware(React.Component) {
             <Link className="left account page-link" to="/account" tabIndex="0" data-tooltip="My Account" data-position="bottom left"><RetinaImage src={AccountIcon}/></Link>
             <Link className="right settings page-link" to="/configuration" tabIndex="0" data-tooltip="Configuration" data-position="bottom right"><i className="settings icon"/></Link>
 
-            <WorldMap/>
+            <WorldMap locations={GPS} location={this.state.location} className="side"/>
 
             <div className="location-list">
               <div>
@@ -159,6 +199,7 @@ export default class ConnectScreen extends DaemonAware(React.Component) {
               </div>
             </div>
 
+            {/*
             <ConnectButton on={this.state.connect} connectionState={this.state.connectionState} onClick={() => this.handleConnectClick()} hidden={this.state.locationListOpen}/>
 
             <div className="connect-status">
@@ -169,6 +210,7 @@ export default class ConnectScreen extends DaemonAware(React.Component) {
             <div className="location-selector">
               <Location location={this.state.locations[this.state.location]} hideTag={true}/>
             </div>
+            */}
 
             <div id="connection-stats" class="ui two column center aligned grid">
               <div class="column"><div class="ui mini statistic"><div class="value">{humanReadableSize(this.state.bytesReceived)}</div><div class="label">Received</div></div></div>
