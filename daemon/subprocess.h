051673850c741853583f7d15da3748cb53998ba3
Kim Nilsson <kim@cypherpunk.com>
2017-05-18 20:41:07 +0900

    Fix up subprocess interface

--- daemon/subprocess.h
+++ daemon/subprocess.h
@@ -9,7 +9,7 @@
 #include <asio.hpp>
 
 
-class Subprocess : std::enable_shared_from_this<Subprocess>
+class Subprocess : public std::enable_shared_from_this<Subprocess>
 {
 public:
 	enum SubprocessFlags {
@@ -27,7 +27,7 @@ public:
 		Continued,
 		OtherError,
 		Success = 0,
-	}
+	};
 	typedef int Result;
 
 protected:
@@ -39,8 +39,10 @@ protected:
 protected:
 	Subprocess(asio::io_service& io) : _io(io), _state(Constructed), _result(NotStarted) {}
 
+	virtual void DoRun(const std::string& executable, const std::vector<std::string>& args, const std::string& cwd, const std::unordered_map<std::string, std::string>& env, unsigned int flags) = 0;
+
 public:
-	static shared_ptr<Subprocess> Create(asio::io_service& io);
+	static std::shared_ptr<Subprocess> Create(asio::io_service& io);
 
 	virtual ~Subprocess() {}
 
@@ -52,11 +54,22 @@ public:
 	void SetStdErrListener(std::function<void(const asio::error_code&, std::string)> cb) { _on_stderr_line = std::move(cb); }
 
 	// Run an executable as a subprocess; can only be called once.
-	virtual void Run(const std::string& executable, const std::vector<std::string>& args, const std::string& cwd, unsigned int flags) = 0;
+	void Run(const std::string& executable, const std::vector<std::string>& args, const std::string& cwd, const std::unordered_map<std::string, std::string>& env, unsigned int flags) { DoRun(executable, args, cwd, env, flags); }
+	void Run(const std::string& executable, const std::vector<std::string>& args, const std::string& cwd, const std::unordered_map<std::string, std::string>& env) { DoRun(executable, args, cwd, env, 0); }
+	void Run(const std::string& executable, const std::vector<std::string>& args, const std::string& cwd, unsigned int flags) { DoRun(executable, args, cwd, {}, flags); }
+	void Run(const std::string& executable, const std::vector<std::string>& args, const std::unordered_map<std::string, std::string>& env, unsigned int flags) { DoRun(executable, args, {}, env, flags); }
+	void Run(const std::string& executable, const std::vector<std::string>& args, const std::string& cwd) { DoRun(executable, args, cwd, {}, 0); }
+	void Run(const std::string& executable, const std::vector<std::string>& args, const std::unordered_map<std::string, std::string>& env) { DoRun(executable, args, {}, env, 0); }
+	void Run(const std::string& executable, const std::vector<std::string>& args, unsigned int flags) { DoRun(executable, args, {}, {}, flags); }
+	void Run(const std::string& executable, const std::vector<std::string>& args) { DoRun(executable, args, {}, {}, 0); }
+	
 	// Write a string to the subprocess' stdin. Can only be called once the subprocess is running.
-	virtual void WriteToStdIn(const std::string& str) = 0;
+	virtual void WriteToStdIn(std::string data) = 0;
 	// Wait for the subprocess to finish execution, receiving a callback when it does.
 	virtual void AsyncWait(std::function<void(const asio::error_code&, Result result)> cb) = 0;
 	// Wait synchronously for the subprocess to exit.
 	virtual Result Wait() = 0;
+
+	// TODO: Kill etc.
+	virtual void Kill(bool force = false) = 0;
 };
