e07b1e9923904962d688003db6d6a87f4adec784
J. Maurice <j@wiz.biz>
2017-05-22 16:49:38 +0900

    Replace zendesk help center links with new cypherpunk support URLs

--- client/src/web/components/AccountScreen.jsx
+++ client/src/web/components/AccountScreen.jsx
@@ -122,8 +122,8 @@ export default class AccountScreen extends React.Component  {
             </div>
             <div className="pane" data-title="More">
               <div className="setting"><Link to="/account/share" tabIndex="0">Share Cypherpunk with a Friend</Link></div>
-              <div className="setting"><ExternalLink href="https://cypherpunk.zendesk.com/hc/requests/new"><div>Open a Support Ticket<small>Your satisfaction is important to us!</small></div></ExternalLink></div>
-              <div className="setting"><ExternalLink href="https://support.cypherpunk.com/hc">Go to Help Center</ExternalLink></div>
+              <div className="setting"><ExternalLink href="https://cypherpunk.com/support/request/new"><div>Open a Support Ticket<small>Your satisfaction is important to us!</small></div></ExternalLink></div>
+              <div className="setting"><ExternalLink href="https://cypherpunk.com/support">Go to Help Center</ExternalLink></div>
               <div className="setting"><Link className="logout" to="/login/logout" tabIndex="0" onClick={() => analytics.event('Account', 'logout')}>Sign Out</Link></div>
             </div>
             <div className="links footer">
