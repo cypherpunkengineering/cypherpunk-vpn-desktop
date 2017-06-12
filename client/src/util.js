a6e959ea22687e2c9378040bbfefc69953ba821a
Kim Nilsson <kim@cypherpunk.com>
2017-06-12 20:40:04 +0900

    Get rid of old debug print

--- client/src/util.js
+++ client/src/util.js
@@ -7,7 +7,7 @@ export function eventPromise(emitter, name) {
 
 export function timeoutPromise(promise, delay, timeoutIsSuccess = false) {
   return new Promise((resolve, reject) => {
-    var timeout = setTimeout(() => { console.log("timed out"); if (timeoutIsSuccess) resolve(); else reject(); }, delay);
+    var timeout = setTimeout(() => { if (timeoutIsSuccess) resolve(); else reject(); }, delay);
     promise.then(val => { clearTimeout(timeout); resolve(val); }, reject);
   });
 }
