ce62d1ba2d74cba75eca600068db8cf46a58208f
Kim Nilsson <kim@cypherpunk.com>
2017-07-07 22:15:52 +0900

    Disable remote port setting when encryption is set to Max Freedom

--- client/src/web/components/config/ConnectionSettings.jsx
+++ client/src/web/components/config/ConnectionSettings.jsx
@@ -8,11 +8,17 @@ import { classList } from '../../util.js';
 import { CheckboxSetting, LinkSetting, InputSetting } from './Settings';
 
 export default class ConnectionSettings extends DaemonAware(React.Component) {
+  constructor(props) {
+    super(props);
+    this.daemonSubscribeState({
+      settings: { encryption: true },
+    })
+  }
   render() {
     return (
       <div className="pane" data-title="Connection Settings">
         <LinkSetting name="encryption" to="/configuration/privacy" label="Encryption" formatValue={v => ({ 'default': "Recommended", 'none': "Max Speed", 'strong': "Max Privacy", 'stealth': "Max Freedom" })[v]}/>
-        <LinkSetting name="remotePort" className="advanced" hidden={!this.props.advanced} to="/configuration/remoteport" label="Remote Port" formatValue={v => v.replace(':', ' ').toUpperCase()}/>
+        <LinkSetting name="remotePort" className="advanced" hidden={!this.props.advanced} disabled={this.state.encryption==='stealth'} to="/configuration/remoteport" label="Remote Port" formatValue={v => this.state.encryption === 'stealth' ? 'HTTPS' : v.replace(':', ' ').toUpperCase()}/>
         <InputSetting name="localPort" className="advanced" hidden={!this.props.advanced} label="Local Port"/>
       </div>
     );
