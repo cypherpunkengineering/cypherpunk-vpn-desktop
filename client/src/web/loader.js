07c6431c47605e7570410ec4bcf367151853bec4
Kim Nilsson <kim@cypherpunk.com>
2017-07-08 22:47:49 +0900

    Avoid flickering loading screen

--- client/src/web/loader.js
+++ client/src/web/loader.js
@@ -8,17 +8,19 @@ var loading = true;
 loader.addEventListener('cancel', event => event.preventDefault());
 
 $(loader).dimmer({ closable: false });
-$(loader).dimmer('show');
+$(loader).dimmer('set dimmed');
 loader.showModal();
 
 export default class Loader {
   static show(text) {
     var label = text || "Loading";
-    loading = true;
     loaderLabel.text(label);
-    $(loader).dimmer('show');
-    if (!loader.open) {
-      loader.showModal();
+    if (!loading) {
+      loading = true;
+      $(loader).dimmer('show');
+      if (!loader.open) {
+        loader.showModal();
+      }
     }
   }
   static hide() {
