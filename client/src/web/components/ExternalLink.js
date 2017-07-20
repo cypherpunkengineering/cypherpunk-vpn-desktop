f4b6ba35cc45ac92ee5e6b817bdbbd9c9ce0a6d6
Kim Nilsson <kim@cypherpunk.com>
2017-07-20 18:06:40 +0900

    Enhance ExternalLink to take parameters and relative URLs

--- client/src/web/components/ExternalLink.js
+++ client/src/web/components/ExternalLink.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import { classList } from '../util';
 const { shell } = require('electron').remote;
 
-export default function ExternalLink({ className, href, ...props } = {}) {
+export const WEBSITE_ROOT = 'https://cypherpunk.com';
+
+
+export function ExternalLink({ className, href, params = null, ...props } = {}) {
+  if (href.startsWith('/') && !href.startsWith('//')) {
+    href = WEBSITE_ROOT + href;
+  }
+  if (params && typeof params === 'object') {
+    let prefix = href.indexOf('?') < 0 ? '?' : '&';
+    let query = Object.mapToArray(params, (k,v) => `${k}=${encodeURIComponent(v)}`).join('&');
+    if (query) href = `${href}${prefix}${query}`;
+  }
   return <a className={classList('external', className)} tabIndex="0" onClick={() => { shell.openExternal(href); }} {...props}/>;
 }
+
+export default ExternalLink;
